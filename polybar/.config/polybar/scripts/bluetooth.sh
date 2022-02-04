f565fb5ed5347620b0cbb06632fd27b3f556ea70
meliurwen <meliurwen@gmail.com>
2022-02-04 10:03:46 +0100

    Rewritten polybar's bluetooth script

--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
-then
-  echo "%{F#66ffffff}"
-else
-  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then 
-    echo ""
-  fi
-  echo "%{F#2193ff}"
-fi
+# Usage: ./bluetooth.sh [--toggle]
 
+case $1 in
+    "")
+        if [ "$(bluetoothctl show | grep "Powered: yes" | wc -c)" -eq 0 ]; then
+            printf "%s\n" "%{F#66ffffff}"
+        elif [ "$(printf "%s\n" info | bluetoothctl | grep 'Device' | wc -c)" -eq 0 ]; then 
+            printf "%s\n" ""
+            printf "%s\n" "%{F#2193ff}"
+        else
+            printf "%s\n" "%{F#2193ff}"
+        fi
+    ;;
+    "--toggle")
+        if [ "$(bluetoothctl show | grep "Powered: yes" | wc -c)" -eq 0 ]; then
+            bluetoothctl power on
+        else
+            bluetoothctl power off
+        fi
+    ;;
+    *)
+        printf "Parameter not supported. Aborting...\n"
+        exit 1
+    ;;
+esac
